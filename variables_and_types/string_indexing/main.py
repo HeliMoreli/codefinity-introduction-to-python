f8c2cbad1c92fd51dd802c82a02852f83b3f46b3
HeliMoreli <>
2026-01-08 20:04:13 +0000

    String Indexing

--- variables_and_types/string_indexing/main.py
+++ variables_and_types/string_indexing/main.py
@@ -1,8 +1,18 @@
-store_name = "Green Valley Market"
-length_of_name = len(store_name)
-character_after_space = store_name[6]
+grocery_item = "Grilled Chicken Salad"
+length_of_item = len(grocery_item)
+
+first_char = grocery_item[0]
+second_char = grocery_item[8]
+third_char = grocery_item[16]
+
+last_char1 = grocery_item[-1]
+last_char2 = grocery_item[-7]
+last_char3 = grocery_item[-15]
 
 
-# Testing
-print("Length of store name:", length_of_name)
-print("Character after the space", character_after_space)
+print(grocery_item[0])
+print(second_char)
+print(third_char)
+print(grocery_item[-1])
+print(last_char2)
+print(last_char3)
