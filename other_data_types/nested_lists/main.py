6a124c81bbac41f9eb93b9994d2db3b03a71530b
HeliMoreli <>
2026-01-11 15:36:12 +0000

    Nested Lists

--- other_data_types/nested_lists/main.py
+++ other_data_types/nested_lists/main.py
@@ -1,56 +1,15 @@
-# Define individual grocery items as lists containing details
-bread = ["Bread", 4.80, 3, "Gluten Free"] # Item name, price, quantity, type
-milk = ["Milk", 5.99, 2, "2% Milk"]   # Item name, price, quantity, type
-apple = ["Apple", 1.27, 12, "Fuji"] # Item name, price, quantity, type
-
-# Create the main grocery list that contains these items
-grocery_list = [bread, apple, milk]
-print("Grocery List:" , grocery_list)
-
-# Accessing and printing specific item details using indexing
-print("Item:", grocery_list[2][0]) # Accesses "Milk" title
-print("Price:", grocery_list[2][1]) # Accesses price of a Milk, which is 5.99
-print("Quantity:", grocery_list[2][2]) # Accesses quantity of Milk, which is 2
-print("Type:", grocery_list[2][3]) # Accesses type of Milk, which is "2% Milk"
-
-# Adding a new sublist item to the grocery list
-onion = ["Onions", 1.30, 10, "Yellow"]
-grocery_list.append(onion)
-
-# Removing an item from the grocery list
-grocery_list.remove(bread)
-
-# Sorting the grocery list alphabetically
-grocery_list.sort()
-print("Updated Grocery List:", grocery_list)
-
-
-
-
-
-
 # Creating an inventory
-inventory = ["carrots", "bananas", "apples"]
-
-# Adding a new item
-inventory.append("oranges")
-
-# Removing an outdated item
-inventory.remove("bananas")
-
-# Sorting the inventory
-inventory.sort()
-
-# Checking the result
-print("Updated inventory:", inventory)
-
-my_first_list = ["chalk", "shoes", "shirt"]
-print(my_first_list)
-my_first_list.append("tape")
-print(my_first_list)
-my_first_list.extend(["snack", "mobile phone"])
-print(my_first_list)
-my_first_list.sort()
-print(my_first_list)
-print(my_first_list[3])
-print(my_first_list[-1])
+vegetables = ["tomatoes", "potatoes", "onions"]
+vegetables.remove(vegetables[2])
+print(vegetables)
+if("carrots" not in vegetables):
+    vegetables.append("carrots")
+print(vegetables)
+vegetables.append("cucumbers")
+print(vegetables)
+vegetables.sort()
+print("Updated Vegetable Inventory:", vegetables)
+if("carrots" in vegetables):
+    print("Carrots are already in the list")
+if("cucumbers" in vegetables):
+    print("Cucumbers are already in the list")
